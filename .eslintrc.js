3b7f0166ae3145b00fbaf4a91c19592955682e49
blausues <blausues@gmail.com>
2022-03-02 22:03:26 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: ['airbnb', '@react-native-community'],
+    rules: {
+        'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+        'react/function-component-definition': [
+            2,
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+    },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    trailingComma: "es5",
+    semi: true,
+    tabWidth: 4,
+    endOfLine: 'auto',
 };
